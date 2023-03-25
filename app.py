cdd7cf8dd0327e190ef63c2d6fcaebd7a31d0ab8
Vikas Maurya <vikas.maurya@nagarro.com>
2023-03-25 13:02:55 +0530

    Hackathon Project

--- app.py
+++ app.py
@@ -10,26 +10,41 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        text = request.form["question"]
         response = openai.Completion.create(
             model="text-davinci-002",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(text),
             temperature=0.6,
         )
+        # response = openai.Image.create(
+        #     prompt=animal,
+        #     n=1,
+        #     size="1024x1024"
+        #     )
+        # image_url = response['data'][0]['url']
+        print(response.choices[0].text)
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
+    print(result)
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+def generate_prompt(text):
+    return text
+
+
+
+
+# def generate_prompt(animal):
+#     return """Suggest two names for an animal that is a superhero.
+
+# Animal: Cat
+# Names: Captain Sharpclaw, Agent Fluffball
+# Animal: Dog
+# Names: Ruff the Protector, Wonder Canine
+# Animal: {}
+# Names:""".format(
+#         animal.capitalize()
+#     )
