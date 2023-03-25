e12ce36df4c687c08c370fbf38d95ec801f47d00
Vikas Maurya <vikas.maurya@nagarro.com>
2023-03-25 22:58:33 +0530

    new changes

--- camera.py
+++ camera.py
@@ -49,6 +49,19 @@ emotion_model.load_weights('model.h5')
 cv2.ocl.setUseOpenCL(False)
 
 
+
+
+common_url = 'https://www.youtube.com/watch?v='
+angry = 'x9UHAuyipx8&ab_channel=AcousticMusicCollection'
+disgust = 'iKzRIweSBLA&list=PL7v1FHGMOadDghZ1m-jEIUnVUsGMT9jbH'
+fear = 'UBBHpoW3AKA&list=PLmgutjZvzLyryoakC3VAlDptXy4ChQGC3&ab_channel=T-Series'
+happy = 'JGwWNGJdvx8&list=PLAQ7nLSEnhWTEihjeM1I-ToPDJEKfZHZu&ab_channel=EdSheeran'
+sad = 'UBBHpoW3AKA&list=PLmgutjZvzLyryoakC3VAlDptXy4ChQGC3&ab_channel=T-Series'
+surprise = 'UupJ9yX3_Bg&list=PLp4nDIl7X7Hd-XnuE5mwiaohwcdMXQMF9&ab_channel=TaylorSwift-Topic'
+
+
+
+
 emotion_dict = {0:"Angry",1:"Disgusted",2:"Fearful",3:"Happy",4:"Neutral",5:"Sad",6:"Surprised"}
 music_dist={0:"songs/angry.csv",1:"songs/disgusted.csv ",2:"songs/fearful.csv",3:"songs/happy.csv",4:"songs/neutral.csv",5:"songs/sad.csv",6:"songs/surprised.csv"}
 global last_frame1                                    
@@ -56,6 +69,8 @@ last_frame1 = np.zeros((480, 640, 3), dtype=np.uint8)
 global cap1 
 show_text=[0]
 
+# global start_time
+# start_time = time.time()
 
 ''' Class for calculating FPS while streaming. Used this to check performance of using another thread for video streaming '''
 class FPS:
@@ -145,6 +160,26 @@ class VideoCamera(object):
 			#print("===========================================",music_dist[show_text[0]],"===========================================")
 			#print(df1)
 			cv2.putText(image, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            # exec(code_block)
+			# end_time = time.time()
+			# running_time = end_time - start_time
+			# if True:
+			# 	emotion = emotion_dict[maxindex]
+			# 	if emotion == 'Angry':
+			# 		webbrowser.open(common_url+angry)
+			# 	elif emotion == 'Disgusted':
+			# 		webbrowser.open(common_url+disgust)
+			# 	elif emotion == 'Fearful':
+			# 		webbrowser.open(common_url+fear)
+			# 	elif emotion == 'Happy':
+			# 		webbrowser.open(common_url+happy)
+			# 	elif emotion == 'Sad':
+			# 		webbrowser.open(common_url+sad)
+			# 	elif emotion == 'Neutral':
+			# 		webbrowser.open(common_url+happy)
+			# 	else:
+			# 		print('EMotion Not Detected click the image again')
+				# start_time = time.time()
 			
 		global last_frame1
 		last_frame1 = image.copy()
@@ -154,9 +189,9 @@ class VideoCamera(object):
 		ret, jpeg = cv2.imencode('.jpg', img)
 		return jpeg.tobytes()
 
-def music_rec():
-	# print('---------------- Value ------------', music_dist[show_text[0]])
-	df = pd.read_csv(music_dist[show_text[0]])
-	df = df[['Name','Album','Artist']]
-	df = df.head(15)
-	return df
+# def music_rec():
+# 	# print('---------------- Value ------------', music_dist[show_text[0]])
+# 	df = pd.read_csv(music_dist[show_text[0]])
+# 	df = df[['Name','Album','Artist']]
+# 	df = df.head(15)
+# 	return df
---
+++ static/main.js
@@ -0,0 +1,228 @@
+let happyMedia = {
+    "1": {
+        url: "https://preppykitchen.com/1448-2/",
+        imgUrl: "https://preppykitchen.com/wp-content/uploads/2021/04/Vanilla-Cake-Blog-1.jpg",
+        title: "Learn how to bake!",
+        blurb: "This is a delicious Vanilla cake recipe great for a special occasion, covered in perfectly creamy buttercream!"
+    },
+    "2": {
+        url: "https://www.fluentin3months.com/common-spanish-phrases/",
+        imgUrl: "https://cdn11.bigcommerce.com/s-ey7tq/images/stencil/1280x1280/products/3606/18734/spain-flag__67306.1639690376.jpg?c=2",
+        title: "Pick up a new language!",
+        blurb: "Learn a couple of words in Spanish, with it being one of the most popular languages of the world. This article will help you learn some of the most common phrases that can help you learn faster."
+    },
+    "3": {
+        url: "https://www.mindful.org/how-to-meditate/",
+        imgUrl: "https://cdn.shortpixel.ai/spai/q_lossy+to_avif+ret_img/https://www.mindful.org/content/uploads/how-to-meditate.jpg",
+        title: "Take care of yourself and learn how to meditate",
+        blurb: "Meditation can help you focus and get a clear state of mind. This article will help walk through a couple of tips and steps on how to achieve it."
+    },
+    "4": {
+        url: "https://usapickleball.org/what-is-pickleball/how-to-play/",
+        imgUrl: "https://static01.nyt.com/images/2022/08/26/multimedia/26WNT-PICKLEBALL-WORKOUT1/26WNT-PICKLEBALL-WORKOUT1-videoSixteenByNine3000.jpg",
+        title: "Pick up a new sport!",
+        blurb: "Learn how to play pickleball, a rising sport similar to Tennis and Ping pong. This article will explain the basic rules and steps on how to play pickleball."
+    },
+    "5": {
+        url: "https://www.youtube.com/watch?v=Ux1ECrNDZl4",
+        imgUrl: "https://content.instructables.com/F0I/CKZ8/IBJTJDRL/F0ICKZ8IBJTJDRL.jpg?auto=webp",
+        title: "Learn the cultures of the world!",
+        blurb: "Learn how to make an origami crane. Origami is a Japanese paper folding technique. This video will show you a step by step."
+    }, 
+    "6": {
+        url: "https://parade.com/1204851/kaitlin-vogel/how-to-make-friends/",
+        imgUrl: "https://t3.ftcdn.net/jpg/02/14/36/84/360_F_214368481_jYlhuRT83m1xnnWig3va94rnu1B40W7Z.jpg",
+        title: "How to make some new friends!",
+        blurb: "Few things in life are more important than supportive, close friends. This article will help you with the first few steps and tips of achieving this."
+    }
+}
+let neutralMedia = {
+    "1" : {
+        url: "https://www.livescience.com/what-is-an-equinox.html",
+        imgUrl: "https://cdn.mos.cms.futurecdn.net/qjGEFah2hAyPBFzkfAFKNB-1920-80.jpg.webp",
+        title: "Recent Interesting Science",
+        blurb: "Increase your knowledge of astronomy through this article. This article also allows you to learn about the cultural and historical significances of equinoxes."
+    },
+    "2" : {
+        url: "https://openai.com/research/gpt-4",
+        imgUrl: "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2020/09/Untitled-design-2020-09-03T133902.980.png",
+        title: "AI of Today",
+        blurb: "Read up on the latest developments in the world of artifical intelligence. This article introduces OpenAI's latest development, GPT-4."
+    },
+    "3" : {
+        url: "https://www.masterclass.com/articles/writing-101-what-is-flash-fiction-learn-how-to-write-flash-fiction-in-7-steps",
+        imgUrl: "https://images.ctfassets.net/3s5io6mnxfqz/78sQGMfkBdHUvtolyMp0lz/da6ce92ec2b68bab0840dc3c01f2b719/AdobeStock_113397728.jpeg?w=1920",
+        title: "Pick Up a New Hobby",
+        blurb: "Read on what flash fiction is and how to create it. This article could possibly get you a new hobby."
+    },
+    "4" : {
+        url: "https://www.youtube.com/watch?v=75RjgtZ2tj0",
+        imgUrl: "https://i.ytimg.com/vi/75RjgtZ2tj0/maxresdefault.jpg",
+        title: "The Power of Gratitude",
+        blurb: "This 3-minute video will be an inspirational message for all. It teaches us how far showing our gratitude can take us, and how it can make others feel."
+    },
+    "5" : {
+        url: "https://www.mayoclinic.org/healthy-lifestyle/stress-management/in-depth/positive-thinking/art-20043950",
+        imgUrl: "https://www.mayoclinic.org/-/media/images/mayologo.png",
+        title: "Positive Thinking",
+        blurb: "This article will talk about some tips to effectively deal with stress that life gives you. "
+    },
+    "6" : {
+        url: "hhttps://www.futurelearn.com/info/blog/general/develop-growth-mindset",
+        imgUrl: "https://cdn-wordpress-info.futurelearn.com/wp-content/uploads/What-is-a-growth-mindset__1500-x-750_JPG-606x303.jpg.webp",
+        title: "Growing Mindset",
+        blurb: "This article will talk some steps on how to get a growth mindset, and becoming more open to feedback."
+    }
+}
+let sadMedia = {
+    "1":{
+        url: "https://hbr.org/2018/11/how-to-keep-working-when-youre-just-not-feeling-it",
+        imgUrl: "https://hbr.org/resources/images/article_assets/2018/10/R1806L_SCHNEIDER.png",
+        title: "Fording on",
+        blurb: "This article will talk about some tips on how to keep working and persisting through times of burnout."
+    },
+    "2":{
+        url: "https://www.mindful.org/how-to-meditate/",
+        imgUrl: "https://cdn.shortpixel.ai/spai/q_lossy+to_avif+ret_img/https://www.mindful.org/content/uploads/how-to-meditate.jpg",
+        title: "Take care of yourself and learn how to meditate",
+        blurb: "Meditation can help you focus and get a clear state of mind. This article will help walk through a couple of tips and steps on how to achieve it."
+    },
+    "3":{
+        url: "https://www.youtube.com/watch?v=-SJywvgaJEI",
+        imgUrl: "https://nationaltoday.com/wp-content/uploads/2021/10/Motivation-and-Inspiration-Day.jpg",
+        title: "If you're feeling down",
+        blurb: "This video is an inspirational message for people who are feeling down, with its main purpose to perk up and help viewers move past times of stress."
+    },
+    "4":{
+        url: "https://www.nytimes.com/guides/well/how-to-be-happy ",
+        imgUrl: "https://www.incimages.com/uploaded_files/image/1024x576/getty_501707442_249236.jpg",
+        title: "Happiness tips",
+        blurb: "This article will talk about some tips on how to be in a better mood during the days."
+    },
+    "5":{
+        url: "https://www.healthline.com/health/anxiety/how-to-make-friends-when-you-have-social-anxiety#Whats-at-the-heart-of-social-anxiety",
+        imgUrl: "https://scdcentre.com/wp-content/uploads/the-psychology-of-happiness.jpg",
+        title: "How to make new friends",
+        blurb: "This article will discuss some basic steps in order to create friendships even when you are shy."
+    },
+    "6":{
+        url: "https://my.clevelandclinic.org/health/articles/6392-stress-coping-with-lifes-stressors ",
+        imgUrl: "https://www.berkeleywellbeing.com/uploads/1/9/4/8/19481349/happiness-drawings_orig.jpg",
+        title: "Coping strategies",
+        blurb: "This article will talk about some tips to effectively deal with stress that life gives you."
+    }
+}
+let respondBtn = document.getElementById("respondbtn");
+let moodBtns = document.getElementsByClassName("moodBtn"); 
+let media = document.getElementsByClassName("media")[0];
+
+if (sessionStorage.getItem("mentalScore") == null) {
+    media.style.filter = "blur(10px)";
+    media.style.pointerEvents = "none";
+}
+// sad
+else if (sessionStorage.getItem("mentalScore") != null && parseInt(sessionStorage.getItem("mentalScore")) <= 333) {
+    media.style.filter = "none";
+    document.getElementById("notice").style.display = "none";
+    media.style.pointerEvents = true;
+    document.getElementById("scoreReport").style.display = "block";
+    document.getElementById("score").style.color = "red";
+    document.getElementById("score").innerText = sessionStorage.getItem("mentalScore");
+    let imgs = document.getElementsByClassName("d-block w-100");
+    let titles = document.getElementsByClassName("card-title");
+    let texts = document.getElementsByClassName("card-text");
+    let btns = document.getElementsByClassName("btn btn-primary");
+    let c = 0;
+    for (let i = 1; i <= Object.keys(sadMedia).length; i++) {
+        imgs[c].src = sadMedia[i].imgUrl;
+        titles[c].innerText = sadMedia[i].title;
+        texts[c].innerText = sadMedia[i].blurb;
+        btns[c].href = sadMedia[i].url;
+        c++;
+    }
+}
+// neutral
+else if (sessionStorage.getItem("mentalScore") != null && parseInt(sessionStorage.getItem("mentalScore")) <= 666) {
+    media.style.filter = "none";
+    document.getElementById("notice").style.display = "none";
+    media.style.pointerEvents = true;
+    document.getElementById("scoreReport").style.display = "block";
+    document.getElementById("score").style.color = "orange";
+    document.getElementById("score").innerText = sessionStorage.getItem("mentalScore");
+    let imgs = document.getElementsByClassName("d-block w-100");
+    let titles = document.getElementsByClassName("card-title");
+    let texts = document.getElementsByClassName("card-text");
+    let btns = document.getElementsByClassName("btn btn-primary");
+    let c = 0;
+    for (let i = 1; i <= Object.keys(neutralMedia).length; i++) {
+        imgs[c].src = neutralMedia[i].imgUrl;
+        titles[c].innerText = neutralMedia[i].title;
+        texts[c].innerText = neutralMedia[i].blurb;
+        btns[c].href = neutralMedia[i].url;
+        c++;
+    }
+}
+// happy
+else {
+    media.style.filter = "none";
+    document.getElementById("notice").style.display = "none";
+    media.style.pointerEvents = true;
+    document.getElementById("scoreReport").style.display = "block";
+    document.getElementById("score").style.color = "green";
+    document.getElementById("score").innerText = sessionStorage.getItem("mentalScore");
+    let imgs = document.getElementsByClassName("d-block w-100");
+    let titles = document.getElementsByClassName("card-title");
+    let texts = document.getElementsByClassName("card-text");
+    let btns = document.getElementsByClassName("btn btn-primary");
+    let c = 0;
+    for (let i = 1; i <= Object.keys(happyMedia).length; i++) {
+        imgs[c].src = happyMedia[i].imgUrl;
+        titles[c].innerText = happyMedia[i].title;
+        texts[c].innerText = happyMedia[i].blurb;
+        btns[c].href = happyMedia[i].url;
+        c++;
+    }
+}
+
+var multipleCardCarousel = document.querySelector(
+    "#carouselExampleControls"
+  );
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+    var cardWidth = $(".carousel-item").width();
+    var scrollPosition = 0;
+    $("#carouselExampleControls .carousel-control-next").on("click", function () {
+      if (scrollPosition < carouselWidth - cardWidth * 4) {
+        scrollPosition += cardWidth;
+        $("#carouselExampleControls .carousel-inner").animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+    $("#carouselExampleControls .carousel-control-prev").on("click", function () {
+      if (scrollPosition > 0) {
+        scrollPosition -= cardWidth;
+        $("#carouselExampleControls .carousel-inner").animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+  } else {
+    $(multipleCardCarousel).addClass("slide");
+  }
+
+document.getElementById("other").onclick = (e) => {
+    location.href = "bubblesgame.html";
+}
+document.getElementById("home").onclick = (e) => {
+    location.href = "index.html";
+}
+document.getElementById("product").onclick = (e) => {
+  location.href = "product.html";
+}
+respondBtn.onclick = (e) => {
+    location.href = "questions.html";
+}
+
